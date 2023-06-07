deddfb9df415bbbd981d2b625982529cedf9d83e
18022192013 <3204378573@qq.com>
2023-06-07 13:46:24 +0800

    登录添加动态背景。添加有分页的反馈功能。

--- src/main.js
+++ src/main.js
@@ -3,18 +3,22 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import Vuelidate from 'vuelidate';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+import VueParticles from 'vue-particles';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './style/scss/index.scss';
 
 Vue.config.productionTip = false;
-
+Vue.use(VueParticles);
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin);
 Vue.use(Vuelidate);
+Vue.use(ElementUI);
 Vue.use(VueAxios, axios);
 Vue.prototype.$axios = axios;
 new Vue({
