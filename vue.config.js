d210e10617ae3f00140f96b863118cc72994cb32
thinkerhui <1142200122@qq.com>
2023-06-23 11:27:22 +0800

    管理员端修改为https

--- vue.config.js
+++ vue.config.js
@@ -3,3 +3,15 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
 });
+const path = require('path');
+const fs = require('fs');
+
+module.exports = {
+  devServer: {
+    open: true,
+    https: {
+      cert: fs.readFileSync(path.join(__dirname, 'src/ssl/cert.crt')),
+      key: fs.readFileSync(path.join(__dirname, 'src/ssl/cert.key')),
+    },
+  },
+};
